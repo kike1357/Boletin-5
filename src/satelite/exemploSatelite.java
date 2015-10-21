72291dbf15015eb2bf0d395e46bac708d0e69900
epastorizalorenzo <epastorizalorenzo@atanasoff24.danielcastelao.org>
2015-10-21 11:36:50 +0200

    exemplo

---
+++ src/satelite/exemploSatelite.java
@@ -0,0 +1,13 @@
+
+package satelite;
+
+
+public class exemploSatelite {
+
+public static void main(String[] args) {
+       Satelite satelite1 = new Satelite(5.20,20.30,30.10); 
+       satelite1.verPosicion();
+      
+    }
+    
+}
