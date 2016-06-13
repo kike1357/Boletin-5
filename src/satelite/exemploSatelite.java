c346b19d1013677b85f2d5e5a2f3ecbdb085f238
Feram <feram@feram.co>
2016-06-13 07:37:26 +0000

    Fix typos

--- src/satelite/exemploSatelite.java
+++ src/satelite/exemploSatelite.java
@@ -1,11 +1,11 @@
 
-package satelite;
+package satellite;
 
 
 public class exemploSatelite {
 
 public static void main(String[] args) {
-       Satelite satelite1 = new Satelite(5.20,20.30,30.10); 
+       Satellite satelite1 = new Satellite(5.20,20.30,30.10); 
        satelite1.verPosicion();
       
     }
